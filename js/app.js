2edb323989a611701b2912a4c5950dac97cb9633
Urko <ikumendibe25@lhusurbil.eus>
2025-11-28 09:08:55 +0100

    kamksetak eta prakak hasieratzen

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const arropa = [
+    {
+        produktuIzena: "Kamiseta1",
+        Prezioa: 15,
+        img: "img/kamisetak/kamiseta1.jpeg",
+        category: 'kamisetak'
+    },
+    {
+        produktuIzena: "Kamiseta2",
+        Prezioa: 15,
+        img: "img/kamisetak/kamiseta2",
+        category: 'kamisetak'
+    },
+    {
+        produktuIzena: "Kamiseta3",
+        Prezioa: 15,
+        img: "img/kamisetak/kamiseta3.jpeg",
+        category: 'kamisetak'
+    },
+    {
+        produktuIzena: "Praka1",
+        Prezioa: 25,
+        img: "img/prakak/prakak1.jpeg",
+        category: 'prakak'
+    },
+    {
+        produktuIzena: "Praka2",
+        Prezioa: 25,
+        img: "img/prakak/prakak2.jpeg",
+        category: 'prakak'
+    },
+    {
+        produktuIzena: "Praka3",
+        Prezioa: 25,
+        img: "img/prakak/prakak3.jpeg",
+        category: 'prakak'
+    },
+    {
+        
+    }
+]
